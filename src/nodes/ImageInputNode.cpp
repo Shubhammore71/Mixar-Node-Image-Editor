1da578558b4c9971df245c9573a6550e57ea6af2
Shubham More <shubham.more@iitgn.ac.in>
2025-04-11 22:37:29 +0530

    Optimize image loading logic in ImageInputNode

--- src/nodes/ImageInputNode.cpp
+++ src/nodes/ImageInputNode.cpp
@@ -7,10 +7,20 @@
 #include "ImageInputNode.hpp"
 #include <opencv2/imgcodecs.hpp>
 
+/**
+ * Constructor for ImageInputNode
+ * Initializes the node with a name and creates an output pin for the loaded image
+ */
 ImageInputNode::ImageInputNode() {
     name = "Image Input";
     outputs.emplace_back(Pin{0, "Image"});
 }
+
+/**
+ *  Returns the data type associated with a given pin
+ * The ID of the pin to check
+ * Integer representing the pin type (0 = image type)
+ */
 
 int ImageInputNode::getPinType(int pinId) const {
     return 0; // Assuming all pins are image type
@@ -23,14 +33,21 @@ void ImageInputNode::process() {
     }
 }
 
+/**
+ * Renders the node's UI elements
+ * Displays the current filepath and provides a button to open the file selection dialog
+ */
+
 void ImageInputNode::drawUI() {
     ImGui::Text("Image Input");
     if(ImGui::Button("Load Image")) {
         auto file = pfd::open_file("Select image", ".", {"Image Files", "*.png *.jpg *.jpeg *.bmp"});
+        // If a file was selected (result is not empty)
         if(!file.result().empty()) {
             filepath = file.result()[0];
             process();
         }
     }
+    // Display the current filepath in the UI
     ImGui::Text("%s", filepath.c_str());
 }
